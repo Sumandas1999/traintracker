a956af68337ff4a28c56897ecfa5006f8366cdd9
suman <ku2sumandas1999@gmail.com>
2023-09-17 15:00:36 +0530

    fix: add

---
+++ src/components/main.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import train from "../components/lottiefiles/train.json"
+function Main() {
+    return (
+        <div className='main' >
+            <div className='main-form'>
+                <h1 >Track your train and save time </h1>
+               
+                <div className='input-form'>
+                    <input></input>
+                    <button>search</button>
+                </div>
+            </div>
+            <div className='map'>
+                hello
+            </div>
+        </div>
+    )
+}
+
+export default Main;
---
+++ src/components/navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+        <div className='container '> welcome</div>
+    </div>
+  )
+}
+
+export default Navbar;
